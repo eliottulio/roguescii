f34a8d52eb47119a661bf31a7f5c747e320409e5
Victor Bernard <victor.j.l.bernard@gmail.com>
2020-03-04 09:51:01 +0000

    fixed speed and moving. should now work.

--- player.py
+++ player.py
@@ -9,26 +9,42 @@ class player:
 		self.room_y = room_pos[1];
 		self.hp = 8;
 		self.damage = 1;
-		self.speed = 2;
+		self.speed = 4;
 
 	def update(self, key):
 		self.prev_x = self.x;
 		self.prev_y = self.y;
 		if key == 'KEY_UP':
 			if self.y <= self.speed:
-				self.y = 1;
+				speed_save = self.speed
+				while self.y <= self.speed and self.speed > 1:
+					self.speed -= 1
+				self.y -= self.speed;
+				self.speed = speed_save
 			else:self.y -= self.speed;
 		elif key == 'KEY_RIGHT':
 			if self.x >= 27-2*self.speed:
-				self.x = 27;
+				speed_save = self.speed
+				while self.x > 27-2*self.speed and self.speed > 1:
+					self.speed -= 1
+				self.x += 2*self.speed;
+				self.speed = speed_save
 			else:self.x += 2*self.speed;
 		elif key == 'KEY_DOWN':
 			if self.y >= 8-self.speed:
-				self.y = 8;
+				speed_save = self.speed
+				while self.y > 8-self.speed and self.speed > 1:
+					self.speed -= 1
+				self.y += self.speed;
+				self.speed = speed_save
 			else:self.y += self.speed;
 		elif key == 'KEY_LEFT':
 			if self.x <= 2*self.speed:
-				self.x = 1;
+				speed_save = self.speed
+				while self.x <= 2*self.speed and self.speed > 1:
+					self.speed -= 1
+				self.x -= 2*self.speed;
+				self.speed = speed_save
 			else:self.x -= 2*self.speed;
 		#DIRECTION
 		dx = self.x-self.prev_x
